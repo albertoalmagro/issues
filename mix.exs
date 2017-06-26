9dc8c502f89d01d986c663a12b036b7ef8cd7a54
Alberto Almagro <alberto.almagro@rakuten.com>
2017-06-26 19:11:55 +0200

    Add docs and doctest for Issues.TableFormatter

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule Issues.Mixfile do
     [app:             :issues,
      escript:         escript_config(),
      version:         "0.1.0",
+     name:            "Issues",
+     source_url:      "https://github.com/albertoalmagro/issues",
      elixir:          "~> 1.4",
      build_embedded:  Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -31,7 +33,9 @@ defmodule Issues.Mixfile do
   defp deps do
     [
       httpoison: "~> 0.11.2",
-      poison:    "~> 3.1"
+      poison:    "~> 3.1",
+      ex_doc:    "~> 0.16.1",
+      earmark:   "~> 1.2"
     ]
   end
 
