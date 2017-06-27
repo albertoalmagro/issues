16b93568d1ca5b621d4b5ac9924ea2ee698267f7
Alberto Almagro <alberto.almagro@rakuten.com>
2017-06-28 00:19:24 +0200

    Add ExCoveralls coverate tool

--- mix.exs
+++ mix.exs
@@ -2,15 +2,24 @@ defmodule Issues.Mixfile do
   use Mix.Project
 
   def project do
-    [app:             :issues,
-     escript:         escript_config(),
-     version:         "0.1.0",
-     name:            "Issues",
-     source_url:      "https://github.com/albertoalmagro/issues",
-     elixir:          "~> 1.4",
-     build_embedded:  Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps:            deps()]
+    [
+      app:               :issues,
+      escript:           escript_config(),
+      version:           "0.1.0",
+      name:              "Issues",
+      source_url:        "https://github.com/albertoalmagro/issues",
+      elixir:            "~> 1.4",
+      build_embedded:    Mix.env == :prod,
+      start_permanent:   Mix.env == :prod,
+      test_coverage:     [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      deps:            deps()
+    ]
   end
 
   # Configuration for the OTP application
@@ -32,10 +41,11 @@ defmodule Issues.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      httpoison: "~> 0.11.2",
-      poison:    "~> 3.1",
-      ex_doc:    "~> 0.16.1",
-      earmark:   "~> 1.2"
+      { :httpoison,   "~> 0.11.2" },
+      { :poison,      "~> 3.1" },
+      { :ex_doc,      "~> 0.16.1" },
+      { :earmark,     "~> 1.2" },
+      { :excoveralls, "~> 0.7.0", only: :test }
     ]
   end
 
